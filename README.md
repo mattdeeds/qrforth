bd1598b8cacd07acb87f0d44842993beffa41788
mattdeeds <mattrdeeds@gmail.com>
2023-11-10 09:28:27 -0500

    update

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 wasm-pack build --target web
 python -m http.server
+python NoCacheHTTPServer.py
+twiggy top pkg/qrforth_bg.wasm
+twiggy top target/wasm32-unknown-unknown/release/qrforth.wasm
 
 cargo bloat
 cargo build --target=wasm32-unknown-unknown --release
--- src/lib.rs
+++ src/lib.rs
@@ -1,193 +1,176 @@
-use wasm_bindgen::prelude::*;
+#![no_std]
 
-// #![no_std]
-
-// #[panic_handler]
-// fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
-//     loop {}
-//     core::arch::wasm32::unreachable()
-// }
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-    #[wasm_bindgen(js_namespace = console)]
-    fn read() -> String;
 }
 
-fn tokenize(input: &str) -> Vec<String> {
-    // split by whitespace
-    let mut tokens: Vec<String> = Vec::new();
-    for token in input.split_whitespace() {
-        tokens.push(token.to_string());
-    }
+pub const STACK_SIZE: usize = 256;
 
-    // remove comments
-    if tokens.contains(&"(".to_string()) {
-        let mut i = 0;
-        let mut j = 0;
-        for token in &tokens {
-            if token == "(" {
-                i = tokens.iter().position(|x| x == token).unwrap();
-            }
-            if token == ")" {
-                j = tokens.iter().position(|x| x == token).unwrap();
-            }
-        }
-        tokens.drain(i..j+1);
-    }
+pub static mut STACK: [i32; STACK_SIZE] = [0; STACK_SIZE];
+pub static mut TOP: u8 = 0;
 
-    tokens
+pub static mut DICT: [&'static str; STACK_SIZE] = [""; STACK_SIZE];
+pub static mut DNAME: [&'static str; STACK_SIZE] = [""; STACK_SIZE];
+pub static mut DLOC: [u8; STACK_SIZE] = [0; STACK_SIZE];
+pub static mut DTOP: u8 = 0;
+
+pub static mut INPUT: [u8; STACK_SIZE] = [0; STACK_SIZE];
+pub static mut ITOP: u8 = 0;
+
+pub static mut S: u8 = 0;
+
+// #[wasm_bindgen]
+// pub extern "C" fn stack() {
+//     unsafe {
+//         while S < TOP {
+//             log(STACK[S]);
+//             S += 1;
+//         }
+//         S = 0;
+//     }
+// }
+
+// fn push(n: i32) {
+//     unsafe {
+//         STACK[TOP as usize] = n;
+//         TOP += 1;
+//     }
+// }
+
+// Function to return a pointer to our buffer
+// in wasm memory
+// JS reads from this buffer to display the stack
+#[wasm_bindgen]
+pub fn get_stack_pointer() -> *const i32 {
+  let pointer: *const i32;
+  unsafe {
+    pointer = STACK.as_ptr();
+  }
+
+  return pointer;
 }
-
-fn words(input: &str, stack: &mut Vec<i32>) -> String {
-    // execute matching word
-    match input {
-        "emit" => {
-            // print character
-            let a = stack.pop().unwrap(); // handle underflow
-            format!("{}", a as u8 as char)
-        },
-        // "key" => { 
-        //     let buffer = read();
-        //     stack.push(buffer.chars().next().unwrap() as i32);
-        // },
-        "cr" => {
-            // print newline
-            let cr = "\n";
-            cr.to_string()
-        },
-        "dup" => {
-            // duplicate top of stack
-            let a = stack.pop().unwrap(); // handle underflow
-            stack.push(a);
-            stack.push(a);
-            format!("ok")
-        },
-        "+" => { 
-            // add top two elements of stack
-            let a = stack.pop().unwrap(); // handle underflow
-            let b = stack.pop().unwrap(); // handle underflow
-            stack.push(a + b);
-            format!("ok")
-        },
-        "0=" => {
-            // check if top of stack is 0
-            let a = stack.pop().unwrap(); // handle underflow
-            if a == 0 {
-                stack.push(-1);
-            } else {
-                stack.push(0);
-            }
-            format!("ok")
-        },
-        "nand" => { 
-            // nand top two elements of stack
-            let a = stack.pop().unwrap(); // handle underflow
-            let b = stack.pop().unwrap(); // handle underflow
-            stack.push(!(a & b));
-            format!("ok")
-        },
-        "." => {
-            // print top of stack
-            let a = stack.pop().unwrap(); // handle underflow
-            format!("{}", a)
-        },
-        _ => format!("? : {}", input),
-    
-    }
-}
-
-fn check_dict(dictionary: &Vec<(String,Vec<String>)>, input: &str) -> i32 {
-    for (key, _) in dictionary {
-        if key == input {
-            // return index of word in dictionary
-            return dictionary.iter().position(|x| x.0 == input).unwrap() as i32;
-        }
-    }
-    -1
-}
-
-fn parse(input: &String, stack: &mut Vec<i32>, dictionary: &Vec<(String,Vec<String>)>) -> String {
-    let mut out = String::new();
-    if input.chars().all(char::is_numeric) { 
-        // if input is a number, push it to the stack
-        stack.push(input.parse::<i32>().unwrap());
-        out = format!("ok");
-    } else {
-        let index = check_dict(&dictionary, input);
-        if index != -1 {
-            // if input is a word, execute it
-            for word in &dictionary[index as usize].1 {
-                out = parse(&word, stack, dictionary);
-            }
-        } else {
-            out = words(input, stack);
-        }
-    }
-    out
-}
-
-pub static mut STACK: Vec<i32> = Vec::new();
-pub static mut DICT: Vec<(String,Vec<String>)> = Vec::new();
 
 #[wasm_bindgen]
-pub extern "C" fn forth(input: &str) -> JsValue {
-    let mut out = String::new();
-    // let mut stack: Vec<i32> = Vec::new();
-    // let mut dictionary: Vec<(String,Vec<String>)> = Vec::new();
+pub fn get_stack_top() -> u8 {
+  unsafe {
+    return TOP;
+  }
+}
 
-    // println!("qrforth v0.1.0, 2023 mdeeds, MIT License"); // output this to dom element
-    // log("qrforth v0.1.0, 2023 mdeeds, MIT License");
+// JS writes to this buffer to pass input to forth
+// #[wasm_bindgen]
+// pub fn get_input_buffer_pointer() -> *const u8 {
+//   let pointer: *const u8;
+//   unsafe {
+//     pointer = INPUT.as_ptr();
+//   }
+//   return pointer;
+// }
 
-    // read input from dom element
-    // let input = read();
+// Function to store the passed value at index 0,
+// in our buffer
+#[wasm_bindgen]
+pub fn store_value_in_input(value: u8, index: u8) {
+  unsafe {
+    INPUT[index as usize] = value;
+    ITOP += 1;
+  }
+}
 
-    // Remove trailing newline character
-    // input = input.trim().to_string();
+#[wasm_bindgen]
+pub fn get_stack_at_location(index: u8) -> i32 {
+  unsafe {
+    return STACK[index as usize];
+  }
+}
 
-    if input == "" {
-        out = "ok".to_string();
-    } else {
-        // tokenize input
-        let tokens = tokenize(input);
+pub static mut DEFINE: bool = false;
+pub static mut COUNT: u8 = 0;
 
-        for token in &tokens {
-
-            // exit if we are defining a new word
-            if token == ":" {
-                break;
-
-            // exceute word if it is in the dictionary
-            } else {
-                unsafe {
-                    out = parse(&token, &mut STACK, &DICT);
-                }
-                // out = parse(&token, &mut STACK, &dictionary);        
-            }
-        }
-        // if tokens begins with :, and ends with ;, add to dictionary
-        if tokens[0] == ":" && tokens[tokens.len()-1] == ";" {
-            let mut new_word: Vec<String> = Vec::new();
-            for token in &tokens[2..tokens.len()-1] {
-                new_word.push(token.to_string());
-            }
-            unsafe {
-                DICT.push((tokens[1].to_string(), new_word));
-            }
-            // dictionary.push((tokens[1].to_string(), new_word));
-            out = "ok".to_string();
-        }
-    }
-    
-    // out // output this to dom element
+#[wasm_bindgen]
+pub fn forth() {
     unsafe {
-        out = out + &format!(" | stack: {:?}", STACK);
-    }
-    // let sout = format!(" | stack: {:?}", stack);
-    // out = out + &sout;
-    JsValue::from_str(&out)
-    // log(&out)
-    // format!("ok | stack: {:?}", stack);
+        let input_slice = core::slice::from_raw_parts(INPUT.as_ptr(), ITOP.into());
+        let input = core::str::from_utf8_unchecked(input_slice);
+        log(input);
+        for token in input.split_whitespace() {
+            log(token);
+            // if token == ":" || DEFINE {
+            //     // start defining new word
+            //     DEFINE = true;
+            //     COUNT += 1;
+            // } else if DEFINE && COUNT == 1 {
+            //     // copy word name to dictionary name array
+            //     DNAME[DTOP as usize] = token;
+            //     // and set the DLOC to starting index of word definition which should be DTOP
+            //     DLOC[DTOP as usize] = DTOP;
+            //     // You can lookup the word name in the dictionary name array to get the starting index of the word definition from the DLOC array
+            // } else if DEFINE && token == ";" {
+            //     // end defining new word
+            //     // Store blank space at end of word definition to mark end of word
+            //     DICT[DTOP as usize] = "";
+            //     DTOP += 1;
+
+            //     DEFINE = false;
+            //     COUNT = 0;
+            //     break;
+            // } else if DEFINE && COUNT > 1 {
+            //     // copy word definition to dictionary array
+            //     DICT[DTOP as usize] = token;
+            //     // increment dictionary pointer
+            //     DTOP += 1;
+            // } else 
+            if token.chars().all(char::is_numeric) {
+                // push number to stack
+                STACK[TOP as usize] = token.parse::<i32>().unwrap_or(1000);
+                if TOP < 255 {
+                    TOP += 1;
+                } else {
+                    log("Stack overflow");
+                }
+            } else if token == "+" {
+                // add top two numbers on stack
+                TOP -= 1;
+                STACK[TOP as usize - 1] += STACK[TOP as usize];
+            } else if token == "-" {
+                // subtract top two numbers on stack
+                TOP -= 1;
+                STACK[TOP as usize - 1] -= STACK[TOP as usize];
+            // } else if DNAME.contains(&token) {
+            //     // copy word definition to input buffer
+            //     // let index = DNAME.iter().position(|&s| s == token).unwrap();
+            //     // let mut i = DLOC[index];
+            //     // while DICT[i as usize] != "" {
+            //     //     INPUT[ITOP as usize] = DICT[i as usize];
+            //     //     ITOP += 1;
+            //     //     i += 1;
+            //     // }
+            //     log("found word")
+            } else {
+                log("?")
+            }
+            // } else if let Some(index) = DNAME.iter().position(|&s| s == token) {
+            //     // println!("Found '{}' at index {}", search_str, index);
+            //     // copy word definition to input buffer
+                
+            // } else {
+            //     // println!("'{}' not found in the array", search_str);
+            // }
+        }
+        ITOP = 0;
+    //     // define new word in dictionary
+    //     // remove : and ; and word name from input
+        
+    //     // DICT[DTOP as usize] = input;
+    //     // copy word name to dictionary
+    //     // copy word definition to dictionary
+    //     // increment dictionary pointer
+        
+    } 
+    // fix core::fmt size and panic checking for forth function
+
 }
